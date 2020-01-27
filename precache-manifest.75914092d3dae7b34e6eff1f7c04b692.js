9a247c420a11cdf32d512874fb2f0cebea813d7f
Haeuncs <haeun.developer@gmail.com>
2020-01-27 19:26:06 +0900

    Updates

---
+++ precache-manifest.75914092d3dae7b34e6eff1f7c04b692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451c4bf481f65b8aa0efccfba169e1da",
+    "url": "/Face_Blur/index.html"
+  },
+  {
+    "revision": "4e4ad716850f43dc0711",
+    "url": "/Face_Blur/static/css/main.e970716e.chunk.css"
+  },
+  {
+    "revision": "2bd2e07e8dee9608606d",
+    "url": "/Face_Blur/static/js/2.a8084f21.chunk.js"
+  },
+  {
+    "revision": "4e4ad716850f43dc0711",
+    "url": "/Face_Blur/static/js/main.b63995a4.chunk.js"
+  },
+  {
+    "revision": "235e9f4368fe1c19f69b",
+    "url": "/Face_Blur/static/js/runtime-main.ec1b3413.js"
+  }
+]);
